d3e15e6a72bbffbce3906bb82e6ae37cc01e77e7
Birngruber Dominik <40146720160258@litec.ac.at>
2021-01-23 14:17:12 +0100

    Enabled CORS

--- Dev/Backend/server/app.ts
+++ Dev/Backend/server/app.ts
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 3000;
 
 const index = require("./routes/index");
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
