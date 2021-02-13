8da4c266b5eb33fc992c6fee9e5f2aeb0d2eab25
Lukas Werner <43266108+WerLu@users.noreply.github.com>
2021-02-13 19:51:02 +0100

    added second Graph to Dashboard that displays all statistics of test cases; added 2 datepicker to select the start and end date to filter the graphs and the testrun navbar; added reset button to reset the datepicker; added the abillity to filter the statistics of testcases and testruns by date range

--- Dev/SmartQS/src/webparts/smartqs/components/Dashboard/Dashboard.tsx
+++ Dev/SmartQS/src/webparts/smartqs/components/Dashboard/Dashboard.tsx
@@ -8,29 +8,54 @@ import {
 } from "@pnp/spfx-controls-react/lib/ChartControl";
 import Chart from "chart.js";
 import TestRunModelMin from "../../model/TestRunModelMin";
-import { rgb2hsv } from "office-ui-fabric-react";
+import {
+  DatePicker,
+  DayOfWeek,
+  getTheme,
+  IconButton,
+  IDatePickerStrings,
+  IIconProps,
+  IStackProps,
+  rgb2hsv,
+  Stack,
+} from "office-ui-fabric-react";
 import TestRunModel from "../../model/TestRunModel";
 import DetailedDashboard from "../DetailedDashboard/DetailedDrillDownChart";
 import styles from "./Dashboard.module.scss";
 import { Accordion } from "@pnp/spfx-controls-react/lib/Accordion";
 import DrillDownChart from "../DrillDownChart/DrillDownChart";
-import TestStatisticModel from "../../model/TestStatisticsModel";
+import TestRunStatisticModel from "../../model/TestRunStatisticsModel";
+import TestCaseStatisticModel from "../../model/TestCaseStatisticsModel";
 import TestRunOverview from "../TestRunOverview/TestRunOverview";
 
 interface IDashboardProps {
   teamsContext: any;
 }
 interface IDashboardState {
-  data: Chart.ChartData;
-  options: any;
+  runData: Chart.ChartData;
+  runOptions: any;
+  caseData: Chart.ChartData;
+  caseOptions: any;
+  startDate: string;
+  endDate: string;
 }
+const columnPropsVertical: Partial<IStackProps> = {
+  tokens: { childrenGap: 15 },
+  styles: { root: { width: 500 } },
+};
 
+const columnPropsHorizontal: Partial<IStackProps> = {
+  tokens: { childrenGap: 5 },
+  styles: { root: { width: 400 } },
+};
 class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: {
+      startDate: null,
+      endDate: null,
+      runData: {
         labels: ["Erfolgreich", "Fehlerhaft", "Nicht durchgeführt"],
         datasets: [
           {
@@ -50,10 +75,61 @@ class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
           },
         ],
       },
-      options: {
+      runOptions: {
         title: {
           display: true,
           text: "Test Statistik",
+        },
+        legend: {
+          display: false,
+        },
+        /* tooltips: {
+          mode: "nearest",
+          callbacks: {
+            title: () => "",
+            label: (tooltipItem, data) => {
+              var label = data.datasets[tooltipItem.datasetIndex];
+              console.log(label);
+              return label;
+            },
+          },
+        }, */
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+      caseData: {
+        labels: ["Erfolgreich", "Fehlerhaft", "Optional", "Nicht durchgeführt"],
+        datasets: [
+          {
+            label: "",
+            data: [0, 0, 0, 0],
+            backgroundColor: [
+              "rgba(74, 192, 192, 0.2)",
+              "rgba(254, 99, 132, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(200, 203, 207, 0.2)",
+            ],
+            borderColor: [
+              "rgb(74, 192, 192)",
+              "rgb(254, 99, 132)",
+              "rgb(153, 102, 255)",
+              "rgb(200, 203, 207)",
+            ],
+            borderWidth: 0,
+          },
+        ],
+      },
+      caseOptions: {
+        title: {
+          display: true,
+          text: "Testfall Statistik",
         },
         legend: {
           display: false,
@@ -84,51 +160,245 @@ class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
 
   componentDidMount() {
     if (
-      this.state.data.datasets[0].data[0] == 0 &&
-      this.state.data.datasets[0].data[1] == 0 &&
-      this.state.data.datasets[0].data[2] == 0
+      this.state.runData.datasets[0].data[0] == 0 &&
+      this.state.runData.datasets[0].data[1] == 0 &&
+      this.state.runData.datasets[0].data[2] == 0
     )
-      this.getTestRunMinData();
+      this.getTestRunStatistics();
+    if (
+      this.state.caseData.datasets[0].data[0] == 0 &&
+      this.state.caseData.datasets[0].data[1] == 0 &&
+      this.state.caseData.datasets[0].data[2] == 0
+    )
+      this.getTestCaseStatistics();
   }
 
-  async getTestRunMinData() {
+  componentDidUpdate(prevProps: object, prevState: object) {
+    if (
+      prevState["startDate"] != this.state.startDate ||
+      prevState["endDate"] != this.state.endDate
+    ) {
+      this.getTestCaseStatistics();
+      this.getTestRunStatistics();
+    }
+  }
+
+  async getTestRunStatistics() {
+    const { startDate, endDate } = this.state;
+    const { teamsContext } = this.props;
+
+    let channelID: string = teamsContext ? teamsContext.channelId : "";
     let url: string;
-    if (this.props.teamsContext != null)
+    let requestOptions: any;
+    if (startDate == null) {
+      url = "http://127.0.0.1:3000/getSuccessStatistics/" + channelID;
+      requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+    } else {
       url =
-        "http://127.0.0.1:3000/getSuccessStatisticsByChannel/" +
-        this.props.teamsContext.channelId;
-    else url = "http://127.0.0.1:3000/getSuccessStatistics";
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    };
+        "http://127.0.0.1:3000/getSuccessStatisticsByTimePeriod/" + channelID;
+      requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ startDate, endDate }),
+      };
+    }
 
     fetch(url, requestOptions)
       .then(async (response) => {
-        const body: TestStatisticModel = await response.json();
-        let newData = this.state.data;
+        const body: TestRunStatisticModel = await response.json();
+        let newData = this.state.runData;
         newData.datasets[0].data[0] = body.successful;
         newData.datasets[0].data[1] = body.failed;
         newData.datasets[0].data[2] = body.notDone;
-        this.setState({ data: newData });
+        this.setState({ runData: newData });
+      })
+
+      .catch((rejected) => console.log(rejected));
+  }
+
+  async getTestCaseStatistics() {
+    const { startDate, endDate } = this.state;
+    const { teamsContext } = this.props;
+
+    let channelID: string = teamsContext ? teamsContext.channelId : "";
+    let url: string;
+    let requestOptions: any;
+    if (startDate == null) {
+      url = "http://127.0.0.1:3000/getTestCaseSuccessStatistics/" + channelID;
+      requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+    } else {
+      url =
+        "http://127.0.0.1:3000/getTestCaseSuccessStatisticsByTimePeriod/" +
+        channelID;
+      requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ startDate, endDate }),
+      };
+    }
+
+    fetch(url, requestOptions)
+      .then(async (response) => {
+        const body: TestCaseStatisticModel = await response.json();
+        let newData = this.state.caseData;
+        newData.datasets[0].data[0] = body.successful;
+        newData.datasets[0].data[1] = body.failed;
+        newData.datasets[0].data[2] = body.optional;
+        newData.datasets[0].data[3] = body.notDone;
+        this.setState({ caseData: newData });
       })
 
       .catch((rejected) => console.log(rejected));
   }
 
   public render(): React.ReactElement<IDashboardProps> {
-    const { data, options } = this.state;
+    const {
+      runData,
+      runOptions,
+      caseData,
+      caseOptions,
+      startDate,
+      endDate,
+    } = this.state;
+
+    const DayPickerStrings: IDatePickerStrings = {
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+
+      shortMonths: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+
+      days: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+
+      shortDays: ["S", "M", "T", "W", "T", "F", "S"],
+
+      goToToday: "Go to today",
+      prevMonthAriaLabel: "Go to previous month",
+      nextMonthAriaLabel: "Go to next month",
+      prevYearAriaLabel: "Go to previous year",
+      nextYearAriaLabel: "Go to next year",
+      closeButtonAriaLabel: "Close date picker",
+    };
+    const resetIcon: IIconProps = { iconName: "Cancel" };
 
     return (
       <div className={styles.Dashboard}>
         <Route path="/dashboard">
-          <ChartControl type={ChartType.Bar} data={data} options={options} />
+          <Stack {...columnPropsVertical}>
+            <Stack horizontal {...columnPropsHorizontal}>
+              <DatePicker
+                firstDayOfWeek={DayOfWeek.Monday}
+                strings={DayPickerStrings}
+                placeholder="Start-Datum auswählen"
+                ariaLabel="Select a date"
+                value={startDate ? new Date(startDate) : null}
+                label=""
+                onSelectDate={(e) => {
+                  this.setState({
+                    startDate: new Date(
+                      e.setTime(e.getTime() + 1 * 60 * 60 * 1000)
+                    ).toISOString(),
+                  });
+                }}
+                className={styles.DatePicker}
+              />
+              <IconButton
+                styles={iconButtonStyles}
+                iconProps={resetIcon}
+                ariaLabel="Close popup modal"
+                onClick={() => this.setState({ startDate: null })}
+              />
+            </Stack>
+            <Stack horizontal {...columnPropsHorizontal}>
+              <DatePicker
+                firstDayOfWeek={DayOfWeek.Monday}
+                strings={DayPickerStrings}
+                placeholder="End-Datum auswählen"
+                ariaLabel="Select a date"
+                value={endDate ? new Date(endDate) : null}
+                label=""
+                onSelectDate={(e) => {
+                  this.setState({
+                    endDate: new Date(
+                      e.setTime(e.getTime() + 1 * 60 * 60 * 1000)
+                    ).toISOString(),
+                  });
+                }}
+                className={styles.DatePicker}
+              />
+              <IconButton
+                styles={iconButtonStyles}
+                iconProps={resetIcon}
+                ariaLabel="Close popup modal"
+                onClick={() => this.setState({ endDate: null })}
+              />
+            </Stack>
+          </Stack>
+          <ChartControl
+            type={ChartType.Bar}
+            data={runData}
+            options={runOptions}
+          />
+          <ChartControl
+            type={ChartType.Bar}
+            data={caseData}
+            options={caseOptions}
+          />
           <TestRunOverview
             teamsContext={this.props.teamsContext}
             readonly={true}
+            startDate={startDate}
+            endDate={endDate}
           />
         </Route>
         <Route exact path="/dashboard/drilldown">
@@ -142,3 +412,16 @@ class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
   }
 }
 export default withRouter(Dashboard);
+
+const theme = getTheme();
+const iconButtonStyles = {
+  root: {
+    color: theme.palette.neutralPrimary,
+    marginLeft: "auto",
+    marginTop: "4px",
+    marginRight: "2px",
+  },
+  rootHovered: {
+    color: theme.palette.neutralDark,
+  },
+};
--- Dev/SmartQS/src/webparts/smartqs/model/TestStatisticsModel.ts
+++
@@ -1,5 +0,0 @@
-export default interface TestStatisticModel {
-  successful: number;
-  failed: number;
-  notDone: number;
-}
